b334fe495abf3aed34e85d3a44e9d376601ef7c8
Sagni-prog <sagnialemayehu69@gmail.com>
2022-12-25 17:36:05 -0500

    footer modified

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -1,5 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaFacebookF } from 'react-icons/fa'
+import { FaInstagram } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
+import { FaPinterestP } from 'react-icons/fa'
+import { FaTwitter } from 'react-icons/fa'
+import img from '../assets/images/news-110x110-1.jpg'
 
 const Footer = () => {
   return (
@@ -8,10 +14,10 @@ const Footer = () => {
           <div className='flex gap-2 w-100 py-5'>
 
             <div className='footer-item'>
-              <div className='flex mb-lt '>
+              <div className='flex mb-sm '>
                    <h2 className='font-montserrat font-24 font-weight-700 color-white'>Explore</h2>
               </div>
-              <div className='flex flex-col gap-1 pl-2'>
+              <div className='flex flex-col gap-1 pl-2 mb-l'>
                 <div className='read-more'>
                   <Link className='link font-montserrat font-18 color-white'>
                       Home
@@ -27,6 +33,31 @@ const Footer = () => {
                       Contact
                   </Link>
                </div>
+              </div>
+
+              <div className='follow'>
+                <div className='mb-sm'>
+                <h3 className='font-montserrat font-18 color-white'>Follow us</h3>
+              </div>
+
+              <div className='social-medias flex gap-1'>
+                  <div className='bg-light-dark box flex align-center justfy-center'>
+                      <FaFacebookF className='font-24 color-light-gray' />
+                  </div>
+                  <div className='bg-light-dark box flex align-center justfy-center'>
+                     <FaInstagram className='font-24 color-light-gray' />
+                  </div>
+                  <div className='bg-light-dark box flex align-center justfy-center'>
+                      <MdEmail className='font-24 color-light-gray' />
+                  </div>
+                  <div className='bg-light-dark box flex align-center justfy-center'>
+                      <FaPinterestP  className='font-24 color-light-gray'/>
+                  </div>
+                  <div className='bg-light-dark box flex align-center justfy-center'>
+                      <FaTwitter className='font-24 color-light-gray'/>
+                  </div>
+                </div>
+
               </div>
             </div>
               
@@ -114,7 +145,7 @@ const Footer = () => {
              </div>
             </div>
 
-            <div className='footer-item'>
+            <div className='footer-item3'>
               <div className='flex mb-sm '>
                    <h2 className='font-montserrat font-24 font-weight-700 color-white'>Catagorieis</h2>
               </div>
@@ -125,7 +156,10 @@ const Footer = () => {
                     <Link className='link font-14 color-white font-montserrat p-1' href = '#'>Business</Link>
                 </div>
                 <div className='flex align-center bg-light-dark border-dark mb-1'>
-                    <Link className='link font-14 color-white font-montserrat p-1' href = '#'>Business</Link>
+                    <Link className='link font-14 color-white font-montserrat p-1' href = '#'>Travel</Link>
+                </div>
+                <div className='flex align-center bg-light-dark border-dark mb-1'>
+                    <Link className='link font-14 color-white font-montserrat p-1' href = '#'>Entertainment</Link>
                 </div>
                 <div className='flex align-center bg-light-dark border-dark mb-1'>
                     <Link className='link font-14 color-white font-montserrat p-1' href = '#'>Business</Link>
@@ -150,24 +184,36 @@ const Footer = () => {
                 </div>
                 <div className='flex align-center bg-light-dark border-dark mb-1'>
                     <Link className='link font-14 color-white font-montserrat p-1' href = '#'>Business</Link>
-                </div>
-                <div className='flex align-center bg-light-dark border-dark mb-1'>
-                    <Link className='link font-14 color-white font-montserrat p-1' href = '#'>Business</Link>
-                </div>
-
-                  
+                </div>  
               </div>
-            </div>
-
-         
-      </div>
-
-            <div className='footer-item'>
-              <div className='flex'>
-                   <h2 className='font-montserrat font-24 font-weight-700 color-white'>Explore</h2>
+          </div>
+       </div>
+          <div className='footer-item'>
+              <div className='flex mb-sm'>
+                   <h2 className='font-montserrat font-24 font-weight-700 color-white'>Photos</h2>
               </div>
 
-         
+              <div className='grid grid-col-3 gap-1'>
+                  <div className='footer-gallery'>
+                      <img src= { img } alt = '' />
+                  </div>
+                  <div className='footer-gallery'>
+                      <img src= { img } alt = '' />
+                  </div>
+                  <div className='footer-gallery'>
+                      <img src= { img } alt = '' />
+                  </div>
+                  <div className='footer-gallery'>
+                      <img src= { img } alt = '' />
+                  </div>
+                  <div className='footer-gallery'>
+                      <img src= { img } alt = '' />
+                  </div>
+                  <div className='footer-gallery'>
+                      <img src= { img } alt = '' />
+                  </div>
+              </div>
+
           </div>
         </div>
       </div>
