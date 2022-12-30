fa668ebe21fa6e069b34175aa1717725d1c0f29c
Sagni-prog <sagnialemayehu69@gmail.com>
2022-12-30 16:16:04 -0500

    comment added

--- src/SinglePost/SinglePost.js
+++ src/SinglePost/SinglePost.js
@@ -2,13 +2,17 @@ import React from 'react'
 import Sidebar from '../Sidebar/Sidebar'
 import { MdSchedule } from 'react-icons/md'
 import { IoEyeOutline } from 'react-icons/io5'
+import { FaThumbsUp } from 'react-icons/fa'
+import { FaRegThumbsUp } from 'react-icons/fa'
+import { MdComment } from 'react-icons/md'
 import img1 from '../assets/images/news-800x500-1.jpg'
+import Tags from '../Tags/Tags'
 
 const SinglePost = () => {
   return (
     <div className='container bg-white'>
         <div className='gap-4 flex mt-3'>
-             <div className='w-100 flex flex-col gap-2'>
+             <div className='w-100 flex flex-col gap-2 mb-l'>
                   <div className='flex flex-col gap-2'>
                       <div className='pt-3 w-90'>
                           <h1 className='font-montserrat font-36 font-weight-700 color-black'>
@@ -40,8 +44,8 @@ const SinglePost = () => {
                       <h4 className='font-montserrat font-weight-700 color-black font-16'>Taiwan will extend mandatory military service from four months to one year, President Tsai Ing-wen has said.</h4> 
                   </div>
 
-                  <div className='title w-80'>
-                      <p className='font-montserrat color-dark font-weight font-16 line-height-1-5'>
+                  <div className='title w-80 border-bottom'>
+                      <p className='font-montserrat color-dark font-weight font-16 line-height-1-5 '>
                         The decision comes amid growing tensions with China, which claims the self-ruled island as its territory.<br/><br/>
                         Speaking at a press conference on Tuesday, President Tsai announced new plans to bolster Taiwan's defence in the event of an attack from Beijing.<br/><br/>
                        "Peace will not drop from the sky... Taiwan is on the frontline of authoritarian expansion," she said.<br/><br/>
@@ -56,9 +60,72 @@ const SinglePost = () => {
                         It was believed to have been a major discussion point when Mr Xi met US President Joe Biden at the G20 summit in November. Following the meeting, Mr Biden said he did not believe China would invade Taiwan.<br/><br/>
                         But tensions again ratcheted up on Monday when Taiwan reported one of the biggest Chinese incursions around the island, with 71 Chinese air force planes, including fighter jets and drones, entering Taiwan's so-called air defence identification zone.<br/><br/>
                      </p> 
+
+                     <div className='flex gap-4 mb-md'>
+                        <div className='flex align-center gap-1'>
+                            <FaThumbsUp className='font-24 color-light-gray' />
+                            <p className='font-montserrat font-16 color-light-gray'>199</p>
+                        </div>
+                        <div className='flex align-center gap-1'>
+                            <MdComment className='font-24 color-light-gray' />
+                            <p className='font-montserrat font-16  color-light-gray'>1202</p>
+                        </div>
+                    </div>
                   </div>
+
+                  <div className='flex flex-col gap-3'>
+                         <div className='flex align-top gap-1'>
+                           <div className = "user-image">
+                               <img src = {img1} alt = '' />
+                          </div>
+                          <div className='comment w-60'>
+                              <h5 className='author font-montserrat font-18 color-dark '>
+                                  Mike Van
+                              </h5>
+                              
+
+                              <p className='font-montserrat font-16 color-light-gray line-height-1-5'>
+                              It was believed to have been a major discussion point when Mr Xi met US President Joe Biden at the G20 summit in November. Following the meeting, Mr Biden said he did not believe China would invade
+                              </p>
+                          </div>
+                      </div>
+                         <div className='flex align-top gap-1'>
+                           <div className = "user-image">
+                               <img src = {img1} alt = '' />
+                          </div>
+                          <div className='comment w-60'>
+                              <h5 className='author font-montserrat font-18 color-dark '>
+                                  Mike Van
+                              </h5>
+                              
+
+                              <p className='font-montserrat font-16 color-light-gray line-height-1-5'>
+                              It was believed to have been a major discussion point when Mr Xi met US President Joe Biden at the G20 summit in November. Following the meeting, Mr Biden said he did not believe China would invade
+                              </p>
+                          </div>
+                      </div>
+                         <div className='flex align-top gap-1'>
+                           <div className = "user-image">
+                               <img src = {img1} alt = '' />
+                          </div>
+                          <div className='comment w-60'>
+                              <h5 className='author font-montserrat font-18 color-dark '>
+                                  Mike Van
+                              </h5>
+                              
+
+                              <p className='font-montserrat font-16 color-light-gray line-height-1-5'>
+                              It was believed to have been a major discussion point when Mr Xi met US President Joe Biden at the G20 summit in November. Following the meeting, Mr Biden said he did not believe China would invade
+                              </p>
+                          </div>
+                      </div>
+                  </div>
+
+   
+                 
              </div>
                 <div className='w-col-2 h-100 mr-3 mt-3'>
+                    <Sidebar />
                     <Sidebar />
                 </div>
           </div>
