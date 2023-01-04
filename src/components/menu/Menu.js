ce8436ee3558850a2ea4a1adef51c4c41bbad51f
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-04 23:52:37 +0300

    mobile view done

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -7,50 +7,19 @@ const Menu = () => {
 
   const {state,dispatch} = useContext(NavContext)
   return (
-    <div  className={`${state.show ? 'show': 'hide'} bg-yellow`}>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-      <h1>hello</h1>
-         {/* <ul className='h-100 flex align-center flex-col justify-between gap-2'>
-        
-           <li className='home'>
-             <Link className=' h-100 color-white link font-18 font-montserrat pb-8' to = '/'>Home</Link>
-             </li>
-
-           
-            <li className='nav-link'> 
-                <Link className='link font-16 font-montserrat color-white pb-8' to = '/post'>Single</Link>
-           </li>
-    
-            <li className='nav-link'> 
-                <Link className='link font-16 font-montserrat color-white pb-8' to = ''>Politics</Link>
-           </li>
-
-            <li className='nav-link'> 
-                <Link className='link font-16 font-montserrat color-white pb-8' to = ''>Business</Link>
-           </li>
-
-            <li className='nav-link'> 
-                <Link className='link font-16 font-montserrat color-white pb-8' to = ''>Technology</Link>
-           </li>
-
-            <li className='nav-link'> 
-                <Link className='link font-16 font-montserrat color-white pb-8' to = ''>Sport</Link>
-           </li>
-
-             <li className='nav-link'>
-                  <Link  Link className='link font-16 font-montserrat color-white pb-8' to = ''>Catagory</Link>
-             </li>
-
-             <li className='nav-link'>
-                  <Link  Link className='link font-16 font-montserrat color-white pb-8' to = ''>Gallery</Link>
-             </li>
-
-             <li className='nav-link'>
-                 <Link className='link font-16 font-montserrat color-white pb-8' to = ''>Contact</Link>
-             </li>
-         </ul> */}
+    <div  className={`${state.show ? 'show': 'hide'} bg-gray`}>
+       <div className='flex flex-col align-center justfy-center gap-2'>
+           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+       </div>
     </div>
   )
 }
