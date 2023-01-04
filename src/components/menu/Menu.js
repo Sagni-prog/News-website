9d6a326f71dd8db40f0c62f6da03432febff7946
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-04 23:55:48 +0300

    responsive design...

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -7,7 +7,7 @@ const Menu = () => {
 
   const {state,dispatch} = useContext(NavContext)
   return (
-    <div  className={`${state.show ? 'show': 'hide'} bg-gray`}>
+    <div  className={`${state.show ? 'show border-top': 'hide'} bg-gray`}>
        <div className='flex flex-col align-center justfy-center gap-2'>
            <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
            <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
