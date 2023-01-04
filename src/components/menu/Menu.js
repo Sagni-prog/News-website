18114994b4e031cf5bba8a9d38250ff2d0237bcf
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-05 00:03:06 +0300

    responsive/...

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -7,18 +7,31 @@ const Menu = () => {
 
   const {state,dispatch} = useContext(NavContext)
   return (
-    <div  className={`${state.show ? 'show border-top': 'hide'} bg-gray`}>
+    <div  className={`${state.show ? 'show border-top': 'hide'} bg-gray mt-4`}>
        <div className='flex flex-col align-center justfy-center gap-2'>
-           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
-           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
-           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
-           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
-           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
-           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
-           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
-           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
-           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
-           <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+          <div className='nav-link mt-2'>
+              <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+          </div>
+          <div className='nav-link'>
+              <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+          </div>
+          <div className='nav-link'>
+              <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+          </div>
+          <div className='nav-link'>
+              <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+          </div>
+          <div className='nav-link'>
+              <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+          </div>
+          <div className='nav-link'>
+              <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+          </div>
+          <div className='nav-link'>
+              <Link className='link font-24 font-montserrat color-white pb-8' to = '/post'>Single</Link>
+          </div>
+          
+          
        </div>
     </div>
   )
