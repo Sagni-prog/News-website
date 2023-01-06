072014ed8b8e04b279500f117c27efa4225b5b28
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-06 09:01:31 +0300

    popular and recent news cahnged

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import im3 from '../../assets/images/news-700x435-1.jpg'
 import { MdSchedule } from 'react-icons/md'
@@ -8,8 +8,28 @@ import Subscribe from '../subscribe/Subscribe'
 import Tags from '../Tags/Tags'
 import { MdCalendarViewMonth } from 'react-icons/md'
 import { MdPerson } from 'react-icons/md'
+import './sidebar.css'
 
 const Sidebar = () => {
+
+   const [recent,setRecent] = useState(true)
+   const [popular,setPopular] = useState(false)
+
+   // useEffect(() => {
+   //    console.log("recent =>",recent)
+   //    console.log("popular =>",popular)
+      
+   // },[recent])
+
+   const handleRecent = () => {
+       setPopular(false)
+       setRecent(true)
+   }
+
+   const handlePopular = () => {
+      setRecent(false)
+      setPopular(true)
+   }
   return (
     <div className='resp'>
      <div className='left-resp '>
@@ -25,15 +45,20 @@ const Sidebar = () => {
                        Featured and Popula News
                     </h5>
                </div>
-            <div className='flex align-center justify-center gap-1 bg-white w-100'>
-                <div className='flex align-center border-yellow border-r-9 p-1'>
-                <Link className='link font-14 color-dark font-montserrat' to = ''>Current News</Link>
+            <div className='recent-popular flex align-center justfy-center bg-white'>
+                <div onClick={ handleRecent } className={`${recent ? 'recent-box' : ''} boxx flex align-center  cursor-pointer`}>
+                   <p className={`${recent ? 'color-white' : 'color-dark'} recent link font-16  font-weigth-700 font-montserrat`} to = ''>Current News</p>
                 </div>
-                <Link className='link font-14 color-dark font-montserrat' to = ''>Most Popular</Link>
+
+             <div onClick={ handlePopular } className={`${popular ? 'popular-box' : ''} boxx flex align-center cursor-pointer`}>
+                <p className={`${popular ? 'color-white' : 'color-dark'} popular link font-16 font-weigth-700 font-montserrat`} to = ''>Popular News</p>
             </div>
+         </div>
         </div>
 
-        <div className='flex flex-col gap-2  mb-l'>
+   { 
+       recent &&
+        <div className='recent-news flex flex-col gap-2  mb-l'>
 
         <div className='flex gap-1 sidebar-card bg-light-gray2'>
                   <div className='sidebar-img'>
@@ -43,7 +68,7 @@ const Sidebar = () => {
                <div className='flex flex-col justfy-center gap-2 '>   
                    <div className='side-bar'>
                         <Link className='link font-montserrat font-weight-700 color-dark font-14 text-uppercase'>
-                              Lorem ipsum dolor sit amet elit...
+                              Recent news Lorem ipsum dolor sit amet elit...
                          </Link>
                    </div>     
 
@@ -156,7 +181,134 @@ const Sidebar = () => {
               </div>
 
               </div>
-        </div>
+   }        
+   { 
+      popular &&
+        <div className='popular-news flex flex-col gap-2  mb-l'>
+
+        <div className='flex gap-1 sidebar-card bg-light-gray2'>
+                  <div className='sidebar-img'>
+                      <img src = { im3 } alt = 'im' />
+                  </div>
+
+               <div className='flex flex-col justfy-center gap-2 '>   
+                   <div className='side-bar'>
+                        <Link className='link font-montserrat font-weight-700 color-dark font-14 text-uppercase'>
+                              Popular news lorem ipsum dolor sit amet elit...
+                         </Link>
+                   </div>     
+
+                  <div className='flex align-center gap-2 mb-1 '>
+                     <div className='flex align-center gap-1'>
+                       <MdPerson className='color-light-gray  font-weight font-14' />   
+                          <Link className='link color-light-gray font-weight font-montserrat font-12' to='' >
+                                Mike Van
+                          </Link>
+                      </div>
+                   <div className='flex align-center gap-1'>
+                        <MdSchedule className='color-light-gray font-weight font-14' />
+                            <Link className='link color-light-gray font-weight font-montserrat font-12' to='' >
+                                    Dec 5,2022
+                            </Link>
+                     </div>
+                  </div>
+            
+               </div>
+              </div>
+
+              <div className='flex gap-1 sidebar-card bg-light-gray2'>
+                  <div className='sidebar-img'>
+                      <img className='' src = { im3 } alt = 'im' />
+                  </div>
+
+               <div className='flex flex-col justfy-center gap-2 '>   
+                   <div className='side-bar'>
+                        <Link className='link font-montserrat font-weight-700 color-dark font-14 text-uppercase'>
+                              Popular news lorem ipsum dolor sit amet elit...
+                         </Link>
+                   </div>  
+
+                    <div className='flex align-center gap-2 mb-1 '>
+                        <div className='flex align-center gap-1'>
+                           <MdPerson className='color-light-gray font-14' />   
+                                <Link className='link color-light-gray font-weight font-montserrat font-12' to='' >
+                                    Mike Van
+                                </Link>
+                        </div>
+                     <div className='flex align-center gap-1'>
+                        <MdSchedule className='color-light-gray font-14' />
+                            <Link className='link color-light-gray font-weight font-montserrat font-12' to='' >
+                                    Dec 5,2022
+                          </Link>
+                     </div>
+                  </div>
+                
+               </div>
+              </div>
+
+
+              <div className='flex gap-1 sidebar-card bg-light-gray2'>
+                  <div className='sidebar-img'>
+                      <img src = { im3 } alt = 'im' />
+                  </div>
+
+               <div className='flex flex-col justfy-center gap-2 '>   
+                  <div className='side-bar'>
+                       <Link className='link font-montserrat font-weight-700 color-dark font-14 text-uppercase'>
+                            Popular news orem ipsum dolor sit amet elit...
+                       </Link>
+                   </div>  
+               
+                    <div className='flex align-center gap-2 mb-1 '>
+                      <div className='flex align-center gap-1'>
+                         <MdPerson className='color-light-gray  font-weight font-14' />   
+                            <Link className='link color-light-gray font-weight font-montserrat font-12' to='' >
+                                Mike Van
+                            </Link>
+                        </div>
+                   <div className='flex align-center gap-1'>
+                        <MdSchedule className='color-light-gray font-weight font-14' />
+                            <Link className='link color-light-gray font-weight font-montserrat font-12' to='' >
+                                    Dec 5,2022
+                            </Link>
+                     </div>
+                  </div>
+               </div>
+            </div>
+
+              <div className='flex gap-1 sidebar-card bg-light-gray2'>
+                  <div className='sidebar-img'>
+                      <img src = { im3 } alt = 'im' />
+                  </div>
+
+               <div className='flex flex-col justfy-center gap-2 '>   
+                  <div className='side-bar'>
+                    <Link className='link font-montserrat font-weight-700 color-dark font-14 text-uppercase'>
+                        Lorem ipsum dolor sit amet elit...
+                    </Link>
+                 </div>  
+                
+                <div className='flex align-center gap-2 mb-1 '>
+                  <div className='flex align-center gap-1'>
+                    <MdPerson className='color-light-gray font-weight font-14' />   
+                       <Link className='link color-light-gray font-weight font-montserrat font-12' to='' >
+                          Mike Van
+                      </Link>
+                  </div>
+                   <div className='flex align-center gap-1'>
+                        <MdSchedule className='color-light-gray font-weight font-14' />
+                            <Link className='link color-light-gray font-weight font-montserrat font-12' to='' >
+                                    Dec 5,2022
+                            </Link>
+                     </div>
+                  </div>
+
+               </div>
+              </div>
+
+              </div>
+   }
+          </div>
         </div>
 
 <div className='right-resp'>
