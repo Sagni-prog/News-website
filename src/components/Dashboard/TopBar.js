e5461db9c8ba43bab8e7ef1af96c759e8eb96260
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-13 16:51:59 +0300

    dashboard topbar modified

--- src/components/Dashboard/TopBar.js
+++ src/components/Dashboard/TopBar.js
@@ -1,18 +1,40 @@
 import React from 'react'
 import './topBar.css'
 import { MdOutlineMenu } from 'react-icons/md'
+import Images from '../../constants/Images'
 
 const TopBar = () => {
+
+  const { mars } = Images;
   return (
     <div className='dashboard-topbar w-100 '>
       <div className='w-100 inside flex dashboard-bg-dark'>
        <div className='title-bar container'>
           <h1 className='font-montserrat font-24 color-white pl-3 pt-3'>Admin</h1>
        </div>
-       <div className='leftBar w-100'>
-          <div></div>
-            <MdOutlineMenu className='color-light-gray font-16 mt-2 ml-2' />
-       </div>
+
+       <div className='leftBar w-100 h-100'>
+           <div className='dashboard-container flex justify-between h-100'>
+              <div className='left-content flex align-center gap-3 h-100'>
+                   <MdOutlineMenu className='color-light-gray font-16' />
+
+                   <div className='search'>
+                      <input className='search-field' placeholder='Search here'  />
+                   </div>
+              </div>
+
+                
+                    <div className='right-content flex align-center gap-1 h-100'>
+                       <div className = "topbar-author-img">
+                           <img src = {mars} />
+                       </div>
+                     <div className = "topbar-author-name">
+                        <p className = "font-montserrat font-18 color-white">Mike Van</p>
+                    </div>
+                  </div>  
+              
+            </div>
+         </div>
        </div>
     </div>
   )
