e95b173528e4367bc85bae8f1a5cbcdda3b2a062
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-14 09:05:49 +0300

    dashboard style modified

--- src/components/Dashboard/MainContainer.js
+++ src/components/Dashboard/MainContainer.js
@@ -2,6 +2,12 @@ import React from 'react'
 import TopBar from './TopBar'
 import AddPost from './AddPost'
 import './mainContainer.css'
+import { 
+      BrowserRouter,
+      Routes,
+      Route,
+      Link
+} from 'react-router-dom'
 const MainContainer = () => {
   return (
     <div className='main-container h-100 flex flex-col'>
