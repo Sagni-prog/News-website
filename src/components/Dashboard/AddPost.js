000e3c977b05eddde79bdbc84295c1580bb4c5d4
Sagni-prog <sagnialemayehu69@gmail.com>
2023-01-14 23:49:04 +0300

    input for dashboard added

--- src/components/Dashboard/AddPost.js
+++ src/components/Dashboard/AddPost.js
@@ -1,10 +1,51 @@
-import React from 'react'
+import { React,useState } from 'react'
 import './addPost.css'
 
 const AddPost = () => {
+
+  const [text, setText] = useState("");
+  const [textCode, setTextCode] = useState("");
+  const [code, setCode] =useState();
+
+  let txt = ``; 
+
+
+  const handleEnter = (e) => {
+     if(e.key === 'Enter'){
+      console.log("enter key pressed")
+     }
+     else{
+      console.log("not enter key")
+     }
+  } 
+
+  const handleSubmit = (e) => {
+    let list = []; 
+   
+    e.preventDefault();
+    let t = text.split("\n")
+
+   for(let i = 0; i < t.length; i ++){
+
+    
+      txt = txt +
+      `<p class = "word-break font-18 font-montserrar">${t[i]}</p>`
+
+        
+    }
+           
+            
+
+      setCode(`<div>${txt}</div>`)
+
+      console.log(code)
+  }
+
+
   return (
     <div className='container add-post w-100 bg-white mb-sm'>
-          <form className='dashboard-container flex flex-col gap-2'>
+      <div><div dangerouslySetInnerHTML={{__html: code}} /></div>
+          <form onSubmit= { (e) => handleSubmit(e) } className='dashboard-container flex flex-col gap-2'>
             <div className='add-post-fields flex flex-col gap-1'>
                 <label>Post Title</label>
                 <input className='' />
@@ -34,12 +75,14 @@ const AddPost = () => {
             </div>
             <div className='add-post-fields flex flex-col gap-1'>
                 <label>Post Detail</label>
-                <textarea />
+                <textarea  onChange={ e => setText(e.target.value) } onKeyPress = {e => handleEnter(e)} />
             </div>
             <div className='add-post-fields flex flex-col gap-1'>
                 <label>Tags</label>
                 <input className='' />
             </div>
+
+            <button type='submit'>Submit</button>
             
             </form>
     </div>
